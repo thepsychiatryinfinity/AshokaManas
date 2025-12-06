32483b8434e770fa1aa3ccf73c848594d17a93b4
thepsychiatryinfinity <thepsychiatryinfinity@gmail.com>
2025-12-06 14:35:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+```javascript
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
+    // [https://vitejs.dev/config/](https://vitejs.dev/config/)
+    export default defineConfig({
+      plugins: [react()],
+    })
+    ```
